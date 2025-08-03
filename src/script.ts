8e271ccb7408b228380db4e2ca7800e50c4f2ea7
Laco Papay <lacop@lacop.net>
2025-08-03 14:52:07 +0200

    service worker

--- src/script.ts
+++ src/script.ts
@@ -5,6 +5,19 @@ import $ from 'jquery';
 (window as any).$ = $;
 (window as any).jQuery = $;
 
+// Register service worker
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js')
+      .then(registration => {
+        console.log('SW registered: ', registration);
+      })
+      .catch(registrationError => {
+        console.log('SW registration failed: ', registrationError);
+      });
+  });
+}
+
 declare var Chessboard: any;
 
 let board: any = null;
